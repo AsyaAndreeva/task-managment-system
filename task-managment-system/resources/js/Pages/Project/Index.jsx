3d16afb0ca4c1568c5d551317444efd653290a95
AsyaAndreeva <asiara032@gmail.com>
2025-01-04 11:58:19 +0200

    feat: added create project flow

--- task-managment-system/resources/js/Pages/Project/Index.jsx
+++ task-managment-system/resources/js/Pages/Project/Index.jsx
@@ -6,7 +6,7 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, Link, router } from "@inertiajs/react";
 import TableHeading from "@/Components/TableHeading";
 
-export default function Index({ auth, projects, queryParams = null }) {
+export default function Index({ auth, projects, queryParams = null, success }) {
   queryParams = queryParams || {};
   const searchFieldChanged = (name, value) => {
     if (value) {
@@ -41,17 +41,30 @@ export default function Index({ auth, projects, queryParams = null }) {
     <AuthenticatedLayout
       user={AuthenticatedLayout.user}
       header={
-        <h2
-          className="font-semibold text-xl text-gray-800
+        <div className="flex justify-between items-center">
+          <h2
+            className="font-semibold text-xl text-gray-800
             dark:text-gray-200 leading-tight"
-        >
-          Projects
-        </h2>
+          >
+            Projects
+          </h2>
+          <Link
+            href={route("project.create")}
+            className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600"
+          >
+            Add new
+          </Link>
+        </div>
       }
     >
       <Head title="Projects" />
       <div className="py-12">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          {success && (
+            <div className="bg-emerald-500 py-2 px-4 text-white rounded mb-4">
+              {success}
+            </div>
+          )}
           <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
             <div className="p-6 text-gray-900 dark:text-gray-100">
               <div className="overflow-auto">
@@ -149,8 +162,8 @@ export default function Index({ auth, projects, queryParams = null }) {
                           <img src={project.image_path} style={{ width: 60 }} />
                         </td>
                         <th className="px-3 py-2 text-gray-100 text-nowrap hover:underline">
-                          <Link href={route('project.show', project.id)}>
-                          {project.name}
+                          <Link href={route("project.show", project.id)}>
+                            {project.name}
                           </Link>
                         </th>
                         <td className="px-3 py-2">
