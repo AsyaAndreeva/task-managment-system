d52d41abd40089847e601c644e92ad193c677014
AsyaAndreeva <asiara032@gmail.com>
2025-01-06 16:52:41 +0200

    feat: added dashboard views

--- task-managment-system/resources/js/Pages/Project/Show.jsx
+++ task-managment-system/resources/js/Pages/Project/Show.jsx
@@ -1,6 +1,6 @@
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 import TasksTable from "../Task/TasksTable";
 
 export default function Show({ auth, success, project, tasks, queryParams }) {
@@ -8,12 +8,20 @@ export default function Show({ auth, success, project, tasks, queryParams }) {
     <AuthenticatedLayout
       user={AuthenticatedLayout.user}
       header={
-        <h2
+        <div className="flex items-center justify-between">
+          <h2
           className="font-semibold text-xl text-gray-800
                     dark:text-gray-200 leading-tight"
         >
           {`Projects "${project.name}"`}
         </h2>
+        <Link
+        href={route("project.edit", project.id)}
+        className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600"
+      >
+        Edit
+      </Link>
+        </div>
       }
     >
       <Head title={`Project ${project.name}`} />
